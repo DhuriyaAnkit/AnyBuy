34628e210207f10c9a11eff15cac6a8d89ad2db6
Dhuriya Ankit Subhash <ankitdhuriya281@gmail.com>
2026-09-13 19:50:36 +0530

    fix(backend): correct cookie-parser import for nestjs bootstrap

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
